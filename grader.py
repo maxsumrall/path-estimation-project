ecbcda48814dfb005ffb00759ce987fb737dafb9
Max Sumrall <max.sumrall@neotechnology.com>
2016-05-30 10:09:49 +0200

    Use the full set of biblio queries.
    The biblio dataset queries were converted to csv.
    Using this, we iterate over all queries and submit
    them to the test program and print out the estimate
    and actual number of paths.

--- grader.py
+++ grader.py
@@ -2,13 +2,15 @@ import subprocess
 import os
 
 # choose which example to use
-test_project_location = ["python_example", "java_example", "c_example"][2]
+import time
+
+test_project_location = ["python_example", "java_example", "c_example"][0]
 
 
 # A script to execute the setup and run scripts supplied by the students.
 # All input and output to the students programs are expected to be newline terminated.
 def main():
-    file = "biblio.data.txt"
+    file = "biblio.txt"
     maximum_path_length = "10"
     budget = "1000000"
 
@@ -24,25 +26,25 @@ def main():
     # tasks in the setup script and not in this script.
     cmd = "cd " + test_project_location + " && ./run.sh " + abs_path + " " + maximum_path_length + " " + budget
     print(cmd)
-    child = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+    child = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
 
     # The process must first send the size of the data structure
     size = getResponse(child)
 
-    # The first path, with edge label "0"
-    send("0", child)
-    # The process must respond with it's estimate
-    estimateA = getResponse(child)
-
-    # The second path with edge labels "10, 10"
-    send("2 1", child)
-    # The process must respond with it's estimate
-    estimateB = getResponse(child)
+    queries = open("biblio.csv", "r")
+    line = queries.readline()
+    while line != "":
+        line = line.split(",")
+        path = line[0]
+        actual = line[1]
+        send(path, child)
+        # The process must respond with it's estimate
+        estimate = getResponse(child)
+        print("Path: " + path + " Estimate: " + estimate + " Actual: " + actual)
+        line = queries.readline()
 
     # A signal to the process that there are no more estimates.
     send("", child)
-
-    print("Size: " + size + ", estimate #1: " + estimateA + ", estimate #2: " + estimateB)
 
 
 def send(message, child):
